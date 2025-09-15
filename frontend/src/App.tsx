8f81aba5cfa71acf187c8afc51abef93bed86b95
erienx <mat21mat12mat@gmail.com>
2025-09-15 12:31:17 +0200

    Added a react + tailwind + vite project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-bg justify-center items-center flex h-screen">
+      <p className='text-white text-semibold text-4xl'>Hello world</p>
+    </div>
+  )
+}
+
+export default App
